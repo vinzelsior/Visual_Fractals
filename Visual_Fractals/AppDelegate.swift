e79b6e8ff188a621aa36b9a817546e9136d22b56
Cedric <60925293+vinzelsior@users.noreply.github.com>
2020-10-08 17:07:57 +0200

    Initial Commit

---
+++ Visual_Fractals/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Visual_Fractals
+//
+//  Created by Cedric Zwahlen on 08.10.20.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Visual_Fractals/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Visual_Fractals
+//
+//  Created by Cedric Zwahlen on 08.10.20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
